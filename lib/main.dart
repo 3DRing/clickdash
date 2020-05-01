f8a0b191d175e8b9dc5dc9d322dc1c0291f7d827
Sergey Koltsov <ringov@yandex-team.ru>
2020-05-01 14:55:00 +0300

    Adding bird images.
    (cherry picked from commit e45f3810b3ffdfd5536387984ef149b5a4dade3c)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 const appName = 'ClickDash';
 
+class Assets {
+  static const dashBlueImage = 'assets/dash_blue.png';
+  static const dashGreenImage = 'assets/dash_green.png';
+
+  const Assets._();
+}
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
