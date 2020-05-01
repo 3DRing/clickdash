6e3599e5ecf4a440beaf93d98751715850c5e196
Sergey Koltsov <ringov@yandex-team.ru>
2020-05-01 15:16:39 +0300

    Adding BirdStoreView.

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,19 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: BirdView(bird: Bird(BirdType.constant)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(child: Container()),
+            BirdStoreView(
+              items: [
+                Bird(BirdType.constant),
+                Bird(BirdType.random),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -66,5 +77,30 @@ class BirdView extends StatelessWidget {
         return Assets.dashGreenImage;
     }
     return Assets.dashBlueImage;
+  }
+}
+
+class BirdStoreView extends StatelessWidget {
+  final List<Bird> items;
+
+  const BirdStoreView({
+    Key key,
+    @required this.items,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: items.map((item) => BirdView(bird: item)).toList(),
+        ),
+      ),
+    );
   }
 }
