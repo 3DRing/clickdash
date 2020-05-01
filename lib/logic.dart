fc7be9545f7434ce363bb72ff2108010be9a0b0b
Sergey Koltsov <ringov@yandex-team.ru>
2020-05-01 14:05:02 +0300

    Testing random via seed.

--- lib/logic.dart
+++ lib/logic.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Bird {
   final BirdType type;
 
@@ -10,12 +12,19 @@ enum BirdType {
 }
 
 class BirdCalc {
+  static const _minRandomValue = 1;
+  static const _maxRandomValue = 50;
+
+  final Random random;
+
+  const BirdCalc(this.random);
+
   int getTransaction(Bird bird) {
-    switch(bird.type) {
+    switch (bird.type) {
       case BirdType.constant:
         return 1;
       case BirdType.random:
-        break;
+        return random.nextInt(_maxRandomValue) + _minRandomValue;
     }
     return 0;
   }
--- test/birdcalc_test.dart
+++ test/birdcalc_test.dart
@@ -1,10 +1,13 @@
+import 'dart:math';
+
 import 'package:clickdash/logic.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('when const bird type then transaction is constant', () {
     // arrange
-    final calc = BirdCalc();
+    final random = Random();
+    final calc = BirdCalc(random);
     final bird = Bird(BirdType.constant);
 
     // act
@@ -12,5 +15,18 @@ void main() {
 
     // assert
     expect(result, 1);
+  });
+
+  test('when random bird type then transaction is dependant on random', () {
+    final random = Random(1);
+    final calc = BirdCalc(random);
+    final bird = Bird(BirdType.random);
+    final expectedResults = [5, 38, 46, 10];
+
+    for (final value in expectedResults) {
+      final result = calc.getTransaction(bird);
+
+      expect(result, value);
+    }
   });
 }
