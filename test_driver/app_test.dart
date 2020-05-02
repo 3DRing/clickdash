f4ad750ed36da695072ec7a720c1d2e39ff7e399
Sergey Koltsov <ringov@yandex-team.ru>
2020-05-02 11:48:13 +0300

    Two more integration tests.

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   FlutterDriver driver;
-
+  final constantItemFinder = find.byValueKey('BirdStoreViewBirdType.constant');
   final firstBirdFinder = find.byValueKey('MainPageBirdType.constant0');
   final balanceFinder = find.byValueKey('WalletViewbalance');
 
@@ -29,5 +29,40 @@ void main() {
 
     final actualBalance = await driver.getText(balanceFinder);
     expect(actualBalance, '\$3');
+  });
+
+  test('when balance less then 25 cannot tap constant bird', () async {
+    await driver.tap(constantItemFinder);
+
+    var actualBalance = await driver.getText(balanceFinder);
+    expect(actualBalance, '\$3');
+
+    for (var i = 0; i < 21; i++) {
+      await driver.tap(firstBirdFinder);
+    }
+
+    await driver.tap(constantItemFinder);
+
+    actualBalance = await driver.getText(balanceFinder);
+    expect(actualBalance, '\$24');
+  });
+
+  test('when balance 25 then can buy constant bird and earn with it', () async {
+    await driver.tap(firstBirdFinder);
+    var actualBalance = await driver.getText(balanceFinder);
+    expect(actualBalance, '\$25');
+
+    await driver.tap(constantItemFinder);
+    actualBalance = await driver.getText(balanceFinder);
+    expect(actualBalance, '\$0');
+
+    final secondBirdFinder = find.byValueKey('MainPageBirdType.constant1');
+
+    await driver.waitFor(secondBirdFinder,
+        timeout: Duration(milliseconds: 200));
+
+    await driver.tap(secondBirdFinder);
+    actualBalance = await driver.getText(balanceFinder);
+    expect(actualBalance, '\$1');
   });
 }
